a72b5546b35e73988c699c0354a8d6d433026cba
Tomasz Klocek <tomek@klocek.info>
2019-11-27 18:44:22 +0100

    first run

---
+++ goalpost-app/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Tomek Klocek on 2019-11-27.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
